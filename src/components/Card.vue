27f98bbfec22024145e5ce6845153caad7f5bf08
Webappmaster-Eugene <johnn.hotmail@mail.ru>
2023-12-29 11:13:08 +0300

    release final project

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,29 +2,43 @@
 defineProps({
   id: Number,
   title: String,
-  img: String,
+  imageUrl: String,
   price: Number,
   isFavorite: Boolean,
   isAdded: Boolean,
-  onClickFavorite: Function
+  onClickFavorite: Function,
+  onClickAdd: Function
 })
 </script>
 
 <template>
   <div
-    class="relative flex flex-col w-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+    class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
-    <div @click="onClickFavorite" class="absolute top-8 left-8">
-      <img :src="isFavorite ? '/like-2.svg' : '/like-1.svg'" alt="Favorite" />
-    </div>
-    <img :src="img" class="w-full" alt="Sneaker" />
-    <p>{{ title }}</p>
+    <img
+      v-if="onClickFavorite"
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like 1"
+      class="absolute top-8 left-8"
+      @click="onClickFavorite"
+    />
+
+    <img :src="imageUrl" alt="Sneaker" />
+
+    <p class="mt-2">{{ title }}</p>
+
     <div class="flex justify-between mt-5">
-      <div class="flex flex-col gap-2">
-        <span class="text-slate-200">Цена:</span>
-        <span class="font-bold">{{ price }} руб.</span>
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }} руб.</b>
       </div>
-      <img src="/plus.svg" alt="Plus" />
+
+      <img
+        v-if="onClickAdd"
+        @click="onClickAdd"
+        :src="!isAdded ? '/plus.svg' : '/checked.svg'"
+        alt="Plus"
+      />
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,27 +1,27 @@
 <script setup>
-import Card from '@/components/Card.vue'
+import Card from './Card.vue'
 
-const props = defineProps({
-  items: Array
+defineProps({
+  items: Array,
+  isFavorites: Boolean
 })
 
-const emit = defineEmits(['addToFavorite'])
-const onClickFavorite = (item) => {
-  emit('addToFavorite', item)
-}
+const emit = defineEmits(['addToFavorite', 'addToCart'])
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-10 mt-5">
+  <div class="grid grid-cols-4 gap-5" v-auto-animate>
     <Card
       v-for="item in items"
       :key="item.id"
       :id="item.id"
       :title="item.title"
+      :imageUrl="item.imageUrl"
       :price="item.price"
-      :img="item.imageUrl"
-      :onClickFavorite="() => onClickFavorite(item)"
-      :isFavorite="item?.isFavorite && item?.isFavorite === true ? true : false"
+      :onClickFavorite="isFavorites ? null : () => emit('addToFavorite', item)"
+      :onClickAdd="isFavorites ? null : () => emit('addToCart', item)"
+      :isFavorite="item.isFavorite"
+      :isAdded="item.isAdded"
     />
   </div>
 </template>
